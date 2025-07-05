7ada3e2109189d534f41db24ee7e536ff0bd1917
Shashank-712 <shashankrawat902@gmail.com>
2025-07-05 21:38:36 +0530

    Blogging Platform

---
+++ blog/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include 
+from . import views
+urlpatterns = [
+   path ('login/',views.user_login, name='login'),
+   path ('register/',views.register, name='register'),
+   path ('logout/',views.user_logout, name='logout'),
+   ]
